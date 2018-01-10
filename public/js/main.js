972b7884186bdb0ab72106020bfba9f66150435c
Milok <milok.murlika@yandex.ua>
2018-01-10 12:06:14 +0200

    Добавил js логику для ш "Окно для инвестора"

--- public/js/main.js
+++ public/js/main.js
@@ -10,24 +10,40 @@ let vm = new Vue({
 				ukr: 'Укр'
 			}
 		},
-		langSwitcherOpen: false
+		langSwitcherOpen: false,
+
+		investorOpen: 0
 	}
 });
 
 let sliderPriory = new Swiper ('.swiper-container[data-slider="priory"]', {
+	effect: 'fade',
 	slidesPerView: 1,
 	loop: true,
 	pagination: {
 		el: '.swiper-pagination[data-pagination="priory"]',
 		clickable: true
+	},
+	autoplay: {
+		delay: 12000
+	},
+	fadeEffect: {
+		crossFade: true
 	}
 });
 
 let sliderSuccessStory = new Swiper ('.swiper-container[data-slider="story"]', {
+	effect: 'fade',
 	slidesPerView: 1,
 	loop: true,
 	pagination: {
 		el: '.swiper-pagination[data-pagination="story"]',
 		clickable: true
+	},
+	autoplay: {
+		delay: 12000
+	},
+	fadeEffect: {
+		crossFade: true
 	}
 });
